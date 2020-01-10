191b4e770bac579e3170bee2562cee13d18af89b
lijinyuan_sz <lijinyuan@sz.baidu.com>
2020-01-10 18:17:50 +0800

    连接mysql

--- app.js
+++ app.js
@@ -4,7 +4,45 @@ const bodyParser = require('koa-bodyparser');
 // 导入controller middleware:
 const controller = require('./controller');
 
+const Sequelize = require('sequelize');
+const config = require('./config');
+
 const app = new Koa();
+
+let sequelize = new Sequelize(config.database, config.username, config.password, {
+  host: config.host,
+  dialect: 'mysql',
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 30000
+  }
+});
+
+let User = sequelize.define('userinfo', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true
+  },
+  userid: Sequelize.INTEGER,
+  username: Sequelize.STRING(50),
+  pwd: Sequelize.STRING(50),
+}, {
+  freezeTableName: true,
+  timestamps: false
+});
+
+(async () => {
+  let user = await User.findAll({
+    where: {
+      username: 'dranein'
+    }
+  });
+  console.log(`find ${user.length} pets:`);
+  for (let p of user) {
+    console.log(JSON.stringify(p));
+  }
+})();
 
 // log request URL:
 app.use(async (ctx, next) => {
