d1e51e42b7ce740de1f1ee313833577285c22b57
lijinyuan_sz <lijinyuan@sz.baidu.com>
2020-03-10 15:39:13 +0800

    config

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const config = {
+const configLijinyuanPc = {
   database: 'shenfen_manage', // 数据库名称
   username: 'root', // 用户名
   password: 'admin', // 口令
@@ -6,12 +6,24 @@ const config = {
   port: 3306 // 端口号，MySQL默认3306
 };
 
-// const config = {
-//   host: 'localhost', // 主机名
-//   database: 'shenfen_manage', // 数据库名称
-//   username: 'root', // 用户名
-//   password: 'admin', // 口令
-//   port: 3306 // 端口号，MySQL默认3306
-// };
+const configLijinyuanHome = {
+  host: 'localhost', // 主机名
+  database: 'shenfen_manage', // 数据库名称
+  username: 'root', // 用户名
+  password: 'admin', // 口令
+  port: 3306 // 端口号，MySQL默认3306
+};
 
-module.exports = config;
+const configClound = {
+  host: 'localhost', // 主机名
+  database: 'shenfen_manage', // 数据库名称
+  username: 'root', // 用户名
+  password: 'Dranein159', // 口令
+  port: 3306 // 端口号，MySQL默认3306
+};
+
+// 虚拟机
+// module.exports = configClound;
+
+// lijinyuan公司电脑
+module.exports = configLijinyuanPc;
